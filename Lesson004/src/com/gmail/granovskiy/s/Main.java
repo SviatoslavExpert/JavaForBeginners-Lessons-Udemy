875b864f516ce54a92cf110494f988694d236981
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-04 22:26:22 +0200

    Initial commit

---
+++ Lesson004/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,12 @@
+//  For Loops
+
+package com.gmail.granovskiy.s;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("The value of i is: " + i);
+        }
+    }
+}
