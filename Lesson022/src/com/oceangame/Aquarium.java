252eb3e6476024e0f7ace3b46a97d7a10936b746
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-08 21:58:03 +0200

    Initial commit

---
+++ Lesson022/src/com/oceangame/Aquarium.java
@@ -0,0 +1,4 @@
+package com.oceangame;
+
+public class Aquarium {
+}
---
+++ Lesson022/src/example/App.java
@@ -0,0 +1,13 @@
+//  Packages
+
+package example;
+
+import ocean.*;
+import ocean.plants.Seaweed;
+
+public class App {
+    public static void main(String[] args) {
+        Fish fish1 = new Fish();
+        Seaweed weed = new Seaweed();
+    }
+}
