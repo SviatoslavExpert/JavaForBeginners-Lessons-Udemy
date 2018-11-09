c50eba01456ce2c15e317dea5ad61ab9d5bf2f67
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-09 20:38:59 +0200

    Initial commit

---
+++ Lesson028/src/com/gmail/granovskiy/s/Camera.java
@@ -0,0 +1,11 @@
+package com.gmail.granovskiy.s;
+
+class Camera extends Machine {
+    public void start() {
+        System.out.println("Camera started.");
+    }
+
+    public void snap() {
+        System.out.println("Photo taken.");
+    }
+}
