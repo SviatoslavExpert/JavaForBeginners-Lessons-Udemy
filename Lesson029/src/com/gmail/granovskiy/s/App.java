96f37bb176d04e7de06f64478d2a8efe22f8b596
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-09 20:49:43 +0200

    Initial commit

---
+++ Lesson029/src/com/gmail/granovskiy/s/App.java
@@ -0,0 +1,29 @@
+//  Using Generics
+
+package com.gmail.granovskiy.s;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<String>();
+
+        strings.add("cat");
+        strings.add("dog");
+        strings.add("alligator");
+
+        String animal = strings.get(1);
+
+        System.out.println(animal);
+
+
+        // There can be more than one type argument
+
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+    }
+
+}
+
