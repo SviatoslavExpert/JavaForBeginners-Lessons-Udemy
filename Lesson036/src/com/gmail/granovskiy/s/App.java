46ff0f5c962af9298149c69763d4419d26643bfc
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-11 11:33:13 +0200

    Initial commit

---
+++ Lesson036/src/com/gmail/granovskiy/s/App.java
@@ -0,0 +1,21 @@
+//  Abstract Classes
+
+package com.gmail.granovskiy.s;
+
+public class App {
+
+    public static void main(String[] args) {
+        Camera cam1 = new Camera();
+        cam1.setId(5);
+
+        Car car1 = new Car();
+        car1.setId(4);
+
+        car1.run();
+        System.out.println();
+        cam1.run();
+        
+        //Machine machine1 = new Machine();
+    }
+
+}
---
+++ Lesson036/src/com/gmail/granovskiy/s/Car.java
@@ -0,0 +1,21 @@
+package com.gmail.granovskiy.s;
+
+public class Car extends Machine {
+
+    @Override
+    public void start() {
+        System.out.println("Starting ignition...");
+    }
+
+    @Override
+    public void doStuff() {
+        System.out.println("Driving...");
+    }
+
+    @Override
+    public void shutdown() {
+        System.out.println("Switch off ignition.");
+          
+    }
+
+}
