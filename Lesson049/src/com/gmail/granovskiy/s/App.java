9887ac5827caf06cb14caa26961e89aed9197fea
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-12 16:14:02 +0200

    Initial commit

--- Lesson049/src/com/gmail/granovskiy/s/App.java
+++ Lesson049/src/com/gmail/granovskiy/s/App.java
@@ -2,14 +2,11 @@
 
 package com.gmail.granovskiy.s;
 
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-
 public class App {
-
 
     public static void main(String[] args) {
         /*
