cb8caa982ad93c77736e1e5a044a5e9eabcc0a77
SviatoslavExpert <s.granovskiy@gmail.com>
2018-11-13 10:23:56 +0200

    Initial commit

---
+++ Lesson054/src/com/gmail/granovskiy/s/ReverseAlphabeticalComparator.java
@@ -0,0 +1,11 @@
+package com.gmail.granovskiy.s;
+
+import java.util.Comparator;
+
+class ReverseAlphabeticalComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+        return -s1.compareTo(s2);
+    }
+}
